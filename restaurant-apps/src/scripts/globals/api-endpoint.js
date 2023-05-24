ddf52005d50488f7542f9be9931493a3c72957a9
Jaka Ashputra <67780612+JakaAshputra@users.noreply.github.com>
2023-05-24 15:42:51 +0700

    Delete restaurant-apps directory

--- restaurant-apps/src/scripts/globals/api-endpoint.js
+++
@@ -1,8 +0,0 @@
-import CONFIG from './config';
-
-const API_ENDPOINT = {
-  LIST_RESTO: `${CONFIG.BASE_URL}list`,
-  DETAIL: (id) => `${CONFIG.BASE_URL}detai/${id}`,
-};
-
-export default API_ENDPOINT;
--- restaurant-apps/src/scripts/globals/config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-  KEY: '',
-  BASE_URL: 'https://restaurant-api.dicoding.dev/',
-};
-
-export default CONFIG;
